bccc463a5ea19b1c5c8aee1eb21e5cf129b20d56
spopova13 <38761801+spopova13@users.noreply.github.com>
2018-05-05 16:03:21 +0500

    Add files via upload

---
+++ homework2.php
@@ -0,0 +1,21 @@
+<?php
+    $x = $_GET['x'];
+	$a = 1;
+	$b = 1;
+	$c = 1;
+	        switch ($a>$x) {
+			    case $a>$x:
+			        echo "Задуманное число НЕ входит в числовой ряд";
+				    continue;
+			   	
+			    case $a == $x:
+                    echo "Задуманное число входит в числовой ряд";
+                    continue;
+			    
+			    case $c == $a:
+                case $a++:
+				case $b == $c:
+			    default:
+			        echo "Что-то пошло не так";
+		                     }					 
+?>		
