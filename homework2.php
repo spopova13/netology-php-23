0feb3f98bf1af50b7ae5be3382813f989421ae8e
spopova13 <38761801+spopova13@users.noreply.github.com>
2018-05-10 21:47:38 +0500

    Update homework2.php

--- homework2.php
+++ homework2.php
@@ -3,19 +3,25 @@
 	$a = 1;
 	$b = 1;
 	$c = 1;
-	        switch ($a>$x) {
-			    case $a>$x:
-			        echo "Задуманное число НЕ входит в числовой ряд";
-				    continue;
-			   	
-			    case $a == $x:
-                    echo "Задуманное число входит в числовой ряд";
-                    continue;
-			    
-			    case $c == $a:
-                case $a++:
-				case $b == $c:
-			    default:
-			        echo "Что-то пошло не так";
-		                     }					 
+	                while ($x) {
+		    if ($a > $x) {
+			    echo "Задуманное число НЕ входит в числовой ряд";
+		        break;
+			}elseif ($a == $x){
+		        echo "Задуманное число входит в числовой ряд";
+			    break;
+            }elseif ($c = $a) { 
+			    echo "c равно a";
+				break;
+			}elseif ($a +=1) {	
+				echo "a равно a + 1";
+				break;
+            }elseif ($b = $c) {
+                echo "b равно c";
+                break;				
+		    }else {
+			    echo "Задайте число";
+                break;				
+			}	
+		}					 
 ?>		
