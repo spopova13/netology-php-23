d2eecaed576a996a7e4094d3c329e8ae759e6c0d
spopova13 <38761801+spopova13@users.noreply.github.com>
2018-05-12 19:22:32 +0500

    Update homework2.php

--- homework2.php
+++ homework2.php
@@ -3,25 +3,17 @@
 	$a = 1;
 	$b = 1;
 	$c = 1;
-	                while ($x) {
-		    if ($a > $x) {
-			    echo "Задуманное число НЕ входит в числовой ряд";
-		        break;
-			}elseif ($a == $x){
-		        echo "Задуманное число входит в числовой ряд";
-			    break;
-            }elseif ($c = $a) { 
-			    echo "c равно a";
-				break;
-			}elseif ($a +=1) {	
-				echo "a равно a + 1";
-				break;
-            }elseif ($b = $c) {
-                echo "b равно c";
-                break;				
-		    }else {
-			    echo "Задайте число";
-                break;				
-			}	
-		}					 
+	    while ($x) {
+                if ($a > $x) {
+	            echo "Задуманное число НЕ входит в числовой ряд";
+		    break;
+	        }elseif ($a == $x){
+		     echo "Задуманное число входит в числовой ряд";
+	             break;
+                }else {
+		    $c = $a; 
+		    $a +=1;	
+		    $b = $c;
+		 }	
+		}				 
 ?>		
